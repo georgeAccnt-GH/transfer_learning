c0f7b3b02d0f50cd239c2d1ac5bd54ec76575e33
George Iordanescu <ghiordan@microsoft.com>
2018-04-03 01:56:39 -0700

    first push

---
+++ Code/src/trvis_utils.py
@@ -0,0 +1,17 @@
+### Copyright (C) Microsoft Corporation.  
+
+import os
+import numpy as np
+import pandas as pd 
+
+class trvis_consts(object):
+
+    BASE_INPUT_DIR_list = ['data']
+
+    def __setattr__(self, *_):
+        raise TypeError
+
+
+# os agnostic 'ls' function
+def get_files_in_dir(crt_dir):
+        return( [f for f in os.listdir(crt_dir) if os.path.isfile(os.path.join(crt_dir, f))])
